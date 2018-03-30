a86f026e0ea9f6f383eef17d268f9b53de56320f
Peter Goshulak <pgoshulak@gmail.com>
2018-03-29 22:53:25 -0400

    Add chart title and area functions using CSS grid

--- barchart.js
+++ barchart.js
@@ -1,8 +1,23 @@
-/* $(document).ready(function () {
-  console.log('loaded!');
-});
- */
+function createTitle (titleText) {
+  return $('<h4></h4>').text(titleText);
+}
 
- function barchart (data, options, element) {
-   $('#' + element).html('Id is ' + element);
- }
+function createChartArea (data, options) {
+  // Create grid container
+  var chartArea = $('<div></div>').addClass('grid-container');
+  var gridTemplateColumns = '';
+  data.forEach(function(entry) {
+    gridTemplateColumns += 'auto ';
+
+    var column = $('<div></div>').addClass('grid-item').text(entry.label);
+    chartArea.append(column);
+  });
+  chartArea.css('grid-template-columns', gridTemplateColumns);
+  return chartArea;
+}
+
+function barchart(data, options, element) {
+  var titleElem = createTitle(options.title);
+  var chartElem = createChartArea(data, options);
+  $('#' + element).append(titleElem, chartElem);
+}
